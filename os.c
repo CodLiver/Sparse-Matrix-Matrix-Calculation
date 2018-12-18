e564c66d420e8d6396b79ce133617cd66866fd8e
Cod Liver (C E S) <cem.e.sunal@durham.ac.uk>
2018-12-18 01:29:15 +0000

    Update os.c

--- os.c
+++ os.c
@@ -15,8 +15,7 @@ void basic_sparsemm_sum(const COO, const COO, const COO,
 	void CSRmakerB(struct sortCOO *list1, double *mtxCSRnz, int *mtxCSRn, int *mtxCSRm, COO A);
 	void matrixAdder(int *curr, double *C2Vec, int *mtxAn, double *mtxAnz, int *mtxAm);
 	void mtxGiantAdder(struct sortCOO *list1,struct sortCOO *list2,struct sortCOO *list3,COO A, COO B, COO C, COO *spC);
-	//void dataFiller(COO spC, int *mtxResAn, double *mtxResAnz, int *mtxResAm, int *posC);
-	// void dataFillerv2(COO spC, int *mtxResAn, double *mtxResAnz, int *mtxResAm, int *posC);
+
 	void dataFiller(struct sortCOO *listRes, int *finalNZ, COO C );
 	int qsorter(const void *i, const void *j);
 	struct sortCOO * sortedNormal(COO A);
@@ -29,19 +28,8 @@ void basic_sparsemm_sum(const COO, const COO, const COO,
 		for (int i = 0; i < A->NZ; i++) {
 			mtxCSRnz[i] = list1[i].d;
 			mtxCSRn[i] = list1[i].j;
-			//printf("mtxCSRm[%d]=%d \n",list1[i].i+1,i+1);
 			mtxCSRm[list1[i].i+1]=i+1;
-			//printf("mtxCSRm[%d]=> %d\n",list1[i].i+1,mtxCSRm[list1[i].i+1]);
-			//or get for at a time and then the remained. make it in a separate branch in github.
-
-			//++mtxCSRm[list1[i].i]; //and make the rest calloc with NO +1
 		}
-
-		/*printf("A:\n");
-		for (int i = 0; i < A->m +1; i++) {
-			printf("%d,",mtxCSRm[i]);
-		}
-		printf("new \n");*/
 
 		int sum=0;
 		for (int fixer = 1; fixer < A->m +1; fixer++) {
@@ -52,16 +40,10 @@ void basic_sparsemm_sum(const COO, const COO, const COO,
 				sum=mtxCSRm[fixer];
 			}
 		}
-		/*for (int i = 0; i < A->m +1; i++) {
-			printf("%d,",mtxCSRm[i]);
-		}
-		printf("\n");*/
 	}
 
 	void CSRmakerB(struct sortCOO *list1, double *mtxCSRnz, int *mtxCSRn, int *mtxCSRm, COO A) {
-
 		//maybe dont do cumulative but normal 1,3,1 and eradicate the need of back-dependency
-
 		////#pragma ivdep
 		for (int i = 0; i < A->NZ; i++) {
 			mtxCSRnz[i] = list1[i].d;
@@ -69,15 +51,7 @@ void basic_sparsemm_sum(const COO, const COO, const COO,
 			mtxCSRm[list1[i].i+1]=i+1;
 			//or get for at a time and then the remained. make it in a separate branch in github.
 
-			//++mtxCSRm[list1[i].i]; //and make the rest calloc with NO +1
 		}
-
-		/*printf("B:\n");
-		for (int i = 0; i < A->n +1; i++) {
-			printf("%d,",mtxCSRm[i]);
-		}
-		printf("new \n");*/
-
 		int sum=0;
 		for (int fixer = 1; fixer < A->n +1; fixer++) {
 
@@ -87,10 +61,6 @@ void basic_sparsemm_sum(const COO, const COO, const COO,
 				sum=mtxCSRm[fixer];//
 			}
 		}
-		/*for (int i = 0; i < A->n +1; i++) {
-			printf("%d,",mtxCSRm[i]);
-		}
-		printf("\n");*/
 	}
 
 	void matrixAdder(int *curr, double *C2Vec, int *mtxAn, double *mtxAnz, int *mtxAm) {
@@ -114,17 +84,12 @@ void basic_sparsemm_sum(const COO, const COO, const COO,
 	int Bnz=B->NZ;
 	int Cnz=C->NZ;
 
-
 	double *mtxAnz = malloc(Anz * sizeof(double));//has nz vals
 	int *mtxAm = calloc((A->m + 1) , sizeof(int));//has cumulative els
-	//mtxAm[0] = 0;
-	// mtxAm[A->m] = Anz;
 	int *mtxAn = malloc(Anz * sizeof(int));//col index
-	//
+
 	double *mtxBnz = malloc(Bnz * sizeof(double));
 	int *mtxBm = calloc((B->m + 1) , sizeof(int));
-	//mtxBm[0] = 0;
-	// mtxBm[B->m] = Bnz;
 	int *mtxBn = malloc(Bnz * sizeof(int));
 	//
 	double *mtxCnz = malloc(Cnz * sizeof(double));
@@ -174,9 +139,6 @@ void basic_sparsemm_sum(const COO, const COO, const COO,
 	alloc_sparse(C->m, C->n, posC, spC);//??
 
 	dataFiller(listRes, &posC,*spC);
-	//dataFiller(*spC, mtxResAn, mtxResAnz, mtxResAm, &posC);
-
-	//print_sparse(*spC);
 
 	free(C2Vec);
 	free(listRes);
@@ -192,15 +154,6 @@ void basic_sparsemm_sum(const COO, const COO, const COO,
 
 
 }
-/*void dataFillerv2(COO spC, int *mtxResAn, double *mtxResAnz, int *mtxResAm, int *posC) {
-
-	////#pragma acc parallel loop
-	for (int NZ = 0; NZ < *posC; NZ++) {
-		spC->coords[NZ].i = mtxResAm[NZ];
-		spC->coords[NZ].j = mtxResAn[NZ];
-		spC->data[NZ] = mtxResAnz[NZ];
-	}
-}*/
 
 void dataFiller(struct sortCOO *listRes, int *finalNZ, COO C ){
 
@@ -298,19 +251,16 @@ void optimised_sparsemm(const COO A, const COO B, COO *C)
 	struct sortCOO *listRes=malloc(0* sizeof(struct sortCOO));//(struct sortCOO *)malloc(0* sizeof(struct sortCOO));
 
 // #pragma acc parallel{}
+
 	for (int i = 0; i < A->m; ++i) {//for all cols or NZ.
+
 		for (int rowMaker = mtxCSRm[i]; rowMaker < mtxCSRm[i+1]; ++rowMaker) {
 			vector[mtxCSRn[rowMaker]] = mtxCSRnz[rowMaker];
 		}
-//vector print
-		/*for (int ij	 = 0; ij < A->n; ij++) {
-				printf("%f,",	vector[ij] );
-			}
-			printf("\n");*/
 
-	for (int j = 0; j < B->n; ++j) {//for all cols /////////7to n
+	for (int j = 0; j < B->n; ++j) {
 
-		//printf()
+
 			for (int colMaker = mtxCSCm[j]; colMaker < mtxCSCm[j+1]; ++colMaker) {
 				res+=vector[mtxCSCn[colMaker]]*mtxCSCnz[colMaker];
 			}
@@ -318,7 +268,6 @@ void optimised_sparsemm(const COO A, const COO B, COO *C)
 					listRes=realloc(listRes, (finalNZ+1) * sizeof(struct sortCOO));
 					listRes[finalNZ].i=i;
 					listRes[finalNZ].j=j;
-					//listRes[finalNZ].ij=i* A->n +j;
 					listRes[finalNZ].d=res;
 					++finalNZ;
 					res=0;
@@ -331,33 +280,15 @@ void optimised_sparsemm(const COO A, const COO B, COO *C)
 	alloc_sparse(A->m, B->n, finalNZ, C);
 	dataFiller(listRes, &finalNZ, *C);
 
-
-	//print_sparse(*C);
-
-
 	free(list1);
 	free(list2);
 	free(listRes);
-	//printf("lr EOF\n");
-
 	free(mtxCSRnz);
-
-	//printf("rnz EOF\n");
-
 	free(mtxCSRn);//
-	//printf("r n EOF\n");
-
 	free(mtxCSRm);//
-	//printf("r m EOF\n");
-
 	free(mtxCSCnz);
-
 	free(mtxCSCn);
-	//printf("c n EOF\n");
-
 	free(mtxCSCm);//
-	//printf("c m EOF\n");
-
 	free(vector);
 	printf("EOF\n");
 
